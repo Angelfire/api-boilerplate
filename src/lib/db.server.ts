c65a39514ae41a05c9f529e4225c67d377aa0b31
Andres Bedoya <mortiis.angel@gmail.com>
2024-05-29 18:52:34 -0500

    Fix biome, Add README

--- src/lib/db.server.ts
+++ src/lib/db.server.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
 export const prisma = new PrismaClient({
   log: [
-    { level: 'query', emit: 'event' },
-    { level: 'error', emit: 'stdout' },
-    { level: 'warn', emit: 'stdout' }
-  ]
+    { level: "query", emit: "event" },
+    { level: "error", emit: "stdout" },
+    { level: "warn", emit: "stdout" },
+  ],
 })
 
 // Listen log events for queries
-prisma.$on('query', e => {
+prisma.$on("query", e => {
   console.log(`Query: ${e.query}`)
   console.log(`Params: ${e.params}`)
   console.log(`Duration: ${e.duration}ms`)
