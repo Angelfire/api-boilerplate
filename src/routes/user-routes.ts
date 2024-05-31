404b72367cf3354040cfc742651c9d2da3c034c5
Andres Bedoya <mortiis.angel@gmail.com>
2024-05-31 12:43:29 -0500

    Unify :id and :username GET endpoint

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -3,13 +3,13 @@ import { Router } from "express"
 import {
   createUser,
   getAllUsers,
-  getUserById,
+  getUserByIdentifier,
 } from "../controllers/user-controller"
 
 const router = Router()
 
 router.get("/", getAllUsers)
-router.get("/:id", getUserById)
+router.get("/:identifier", getUserByIdentifier)
 router.post("/", createUser)
 
 export default router
